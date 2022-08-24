01404788a04b4f9945bc06bfc82840194325975b
Dimitri Balzer <38956494+balzerdi@users.noreply.github.com>
2022-08-24 12:50:58 +0200

    Added Notice
    Notice about work in progress added.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Footer from '../components/modules/Footer'
-import { Provider } from 'react-redux'
+import { Provider, useDispatch } from 'react-redux'
 import { store } from '../redux/store'
-import ToastContainer from '../components/atoms/ToastContainer'
 import Header from '../components/modules/Header'
+import { useEffect, useState } from 'react'
+import { add } from '../redux/toastSlices'
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+    useEffect(() => {
+        setTimeout(() => {
+            store.dispatch(add('Please note that this site is still a work in progress.'))
+        }, 2000)
+
+    }, [])
+
     return (
         <Provider store={store}>
             <Header />
